9e63546a6a1960e2de636c05c94233bdad8f4b4d
Nastihan Beheshtypour <nastihanb@gmail.com>
2024-01-04 00:04:01 +0330

    query users array

--- index.js
+++ index.js
@@ -2,15 +2,44 @@ const apolloServer = require('apollo-server');
 const { appendFile } = require('fs');
 const gql = apolloServer.gql;
 
+const users = [
+    {
+        id:1,
+        firstName:"John",
+        lastName:"Paul",
+        email:"john@gmail.com",
+        password:"12345"
+    },
+    {
+        id:2,
+        firstName:"Arthur",
+        lastName:"Morgan",
+        email:"Arthur@gmail.com",
+        password:"54321"
+    }
+]
+
 const typeDefs = gql`
     type Query{
-        greet:String
+        users:[User]
+        user(id:ID!):User
+    }
+
+    type User{
+        id:ID
+        firstName:String
+        lastName:String
+        email:String
     }
 `
 
 const resolvers = {
     Query:{
-        greet:()=> `Hello World!`
+        users:()=>users,
+        user:(parent, {id}, context)=>{
+            console.log(id);
+            return users.find(item=>item.id == id)
+        }
     }
 }
 
